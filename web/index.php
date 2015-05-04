12c8d80fad96ccfa3a305fe30e438b35d1b47707
Jordan Cook <jordan@packethouse.com>
2015-05-04 04:58:25 -0400

    Added composer require recapcha

--- web/index.php
+++ web/index.php
@@ -1,4 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 
-$config = new \CommunityPi\Config\ConfigParse();
+$core = new \CommunityPi\Core;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$payload = $_POST;
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	$payload = $_GET;
+}
+
+$core->requestRouter($_SERVER['REQUEST_URI'], $payload);
+
+?>
